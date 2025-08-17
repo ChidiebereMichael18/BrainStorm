94c35bc258861b5660199ddce756b5254ef38923
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-08-17 11:30:45 +0100

    brainstorm is cooking

--- brainstorm/src/components/dashboard/Navbar.jsx
+++ brainstorm/src/components/dashboard/Navbar.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const Navbar = ({ onMenuToggle }) => {
   const navigate = useNavigate();
   return (
-    <nav className="bg-light shadow-md p-2 flex justify-between items-center">
+    <nav className="bg-gray-900 shadow-md p-2 flex justify-between items-center">
       <div className="flex items-center  font-montserrat">
         <button className="lg:hidden mr-4 " onClick={onMenuToggle}>
           <FaBars className="text-xl text-white" />
@@ -23,13 +23,13 @@ const Navbar = ({ onMenuToggle }) => {
         </div>
       </div>
       <div className="flex items-center  font-montserrat  space-x-4">
-        <div className="w-8 h-8 hover:animate-pulse bg-textHover  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-textHover cursor-pointer rounded-full flex items-center justify-center">
+        <div className="w-8 h-8 hover:animate-pulse bg-green-400  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-textHover cursor-pointer rounded-full flex items-center justify-center">
           <FaPlusCircle className="text-white" size={20} onClick={()=> {navigate('./myposts')}} />
         </div>
-        <div className="w-8 h-8 bg-textHover  transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-textHover cursor-pointer rounded-full flex items-center justify-center">
+        <div className="w-8 h-8 bg-green-400 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-textHover cursor-pointer rounded-full flex items-center justify-center">
           <FaSearch className=" text-white" size={18} onClick={()=> {navigate('./search')}}/>
         </div>
-        <div className="w-8 h-8 bg-textHover hover:animate-pulse cursor-pointer transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-textHover rounded-full flex items-center justify-center">
+        <div className="w-8 h-8 bg-green-400 hover:animate-pulse cursor-pointer transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-textHover rounded-full flex items-center justify-center">
           <FaUser className="text-white" size={18} onClick={()=> {navigate("./profile")}}/>
         </div>
       </div>
